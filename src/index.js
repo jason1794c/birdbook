fbc2ffdf0ba09c0474095bf1e0306a5366bc81f8
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-04-02 12:42:47 -0400

    Remove strictmode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { StateProvider } from './components/StateProvider';
 import reducer, { initialState } from "./components/reducer";
 
 ReactDOM.render(
-  <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
-    </StateProvider>
-  </React.StrictMode>,
+    </StateProvider>,
   document.getElementById('root')
 );
 
